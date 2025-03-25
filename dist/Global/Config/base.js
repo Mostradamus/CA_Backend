0dbedd8b0f25eede92a6ae3f649bd5b0260e6ecd
pomaDevOficial <132797471+pomaDevOficial@users.noreply.github.com>
2025-03-24 23:37:47 -0500

    add

--- dist/Global/Config/base.js
+++ dist/Global/Config/base.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Controllers = void 0;
-const users_ctrl_1 = require("../../Core/Controllers/users.ctrl.js");
-const formats_ctrl_1 = require("../../Core/Controllers/formats.ctrl.js");
-const area_ctrl_1 = require("../../Core/Controllers/area.ctrl.js");
-const turno_ctrl_1 = require("../../Core/Controllers/turno.ctrl.js");
-const reports_ctrl_1 = require("../../Core/Controllers/reports.ctrl.js");
+const users_ctrl_1 = require("../../Core/Controllers/users.ctrl");
+const formats_ctrl_1 = require("../../Core/Controllers/formats.ctrl");
+const area_ctrl_1 = require("../../Core/Controllers/area.ctrl");
+const turno_ctrl_1 = require("../../Core/Controllers/turno.ctrl");
+const reports_ctrl_1 = require("../../Core/Controllers/reports.ctrl");
 exports.Controllers = { UsersControllers: users_ctrl_1.UsersControllers, FormatsControllers: formats_ctrl_1.FormatsControllers, AreaControllers: area_ctrl_1.AreaControllers, TurnoControllers: turno_ctrl_1.TurnoControllers, ReportsControllers: reports_ctrl_1.ReportsControllers };
